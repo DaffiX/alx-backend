46a15948faeb4090d7378c2ef00e748b85b1bfb1
DaffiX <suleydaffy@gmail.com>
2023-08-13 21:54:45 +0300

    simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ The function simplify the process of determine ragne of index
 """
+
+
 def index_range(page, page_size):
     start = (page - 1) * page_size
     end = page * page_size
-    return start, end 
+    return start, end
